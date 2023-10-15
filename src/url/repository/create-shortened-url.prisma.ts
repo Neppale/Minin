5b9465529efc7e3cdc9c849560cfa22a2b171782
Gabriel Verissimo Dias <gabrielverissimodias@gmail.com>
2023-10-15 17:19:04 -0300

    feat: added create shortened url service

--- src/url/repository/create-shortened-url.prisma.ts
+++ src/url/repository/create-shortened-url.prisma.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import { CreateShortenedUrlRepository } from './repository/useCases/create-shortened-url.repository';
+import { CreateShortenedUrlRepository } from './useCases/create-shortened-url.repository';
 import { PrismaClient, Url } from '@prisma/client';
-import { PrismaService } from '../prisma/services/prisma.service';
-import { CreateShortenedUrlParams } from './models/params/create-shortened-url.params';
+import { PrismaService } from '../../prisma/services/prisma.service';
+import { CreateShortenedUrlParams } from '../models/params/create-shortened-url.params';
 
 @Injectable()
 export class CreateShortenedUrlPrisma implements CreateShortenedUrlRepository {
