7e24e4aae65c4cde30ea1360ee7df3daeb3135f4
Gabriel Verissimo Dias <gabrielverissimodias@gmail.com>
2023-10-15 17:53:29 -0300

    feat: added is url decorator to create shortened url dto

--- src/url/models/dto/create-shortened-url.dto.ts
+++ src/url/models/dto/create-shortened-url.dto.ts
@@ -1,7 +1,8 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsNotEmpty, IsString, IsUrl } from 'class-validator';
 
 export class CreateShortenedUrlDto {
   @IsString()
   @IsNotEmpty()
+  @IsUrl()
   url: string;
 }
